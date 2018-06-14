a08be6205d67d2eea4128b48ab41a5600bbcc27c
José Joaquín Arias Gómez-Calcerrada <joaquin.ariasgomez@alum.uca.es>
2018-06-14 17:49:00 +0200

    Improved day/night cycle

---
+++ Unity projects/DayNight cycle test/Assets/LightController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    Vector3 gravityUp;
+    Vector3 bodyUp;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gravityUp = (transform.position - Vector3.zero).normalized;
+        bodyUp = transform.up;
+
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 50 * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (gravityUp != null)
+        {
+            Gizmos.color = Color.white;
+            //Gizmos.DrawLine(Vector3.zero, transform.position);
+        }
+        //Gizmos.color = Color.black;
+        //Gizmos.DrawLine(Vector3.zero, bodyUp);
+    }
+}
