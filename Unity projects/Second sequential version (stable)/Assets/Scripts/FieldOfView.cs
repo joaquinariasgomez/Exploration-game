bd8c855a89a4ec714f29c10f8ac67470e7f17f77
empiremay <tatinarias@hotmail.com>
2019-01-06 18:53:36 +0100

    Field of View visual facility added

---
+++ Unity projects/Second sequential version (stable)/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour {
+
+    public float viewRadius;
+    [Range(0,360)]
+    public float viewAngle;
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if(!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad),0,Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
